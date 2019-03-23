c8991783ab24e08b3e512880562c0b419a84092c
Thomas <thomas.neuraz@8securities.com>
2019-03-23 21:51:22 +0800

    disable warnings

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import RootNavigator from '@core/pages/RootNavigator';
 
 export default class App extends React.Component {
   render() {
+    // disable the warnings
+    console.disableYellowBox = true;
     return (
       <RootNavigator />
     );
